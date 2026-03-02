65ef77c22c153ff996d3c88755fc417f21ce6a45
wizardblue55 <wizardblue55@gmail.com>
2026-03-01 20:24:42 -0600

    remade the whole thing with pygame

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,11 +1,13 @@
 import math
-import tkinter as tk
-from tkinter.font import BOLD
+from sre_constants import JUMP
+import sys
 import pygame
+import pygame.display
 #figuring out importing
+pygame.init()
+debugging = True
 
-root = tk.Tk()
-stump = tk.Tk()
+"""
 print("Hello, World!")
 x = 0
 sevenSix = ["Oliver", "Eliot"]
@@ -15,45 +17,96 @@ while x<100:
     print(sevenSix[x % 2])
     x += 1
 
-def hide(x):
-    x.withdraw()
-    x.update()
-
-def show(x):
-    x.deiconif()
-    x.update()
-
-def onClick():
-    label.config(text="clicked motherfucker!!!!!!")
-
 print(math.pi)
 cunt = int(input())
 chatGPT = math.sqrt(cunt)
 chatGPT = int(chatGPT)
 print(chatGPT)
 #trying out the math class I imported earlier
+"""
 
-#make a window i hope
-#FUCK YEAH BABY IT WORKS1
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
+# Game/Window settings
+WIDTH = 800
+HEIGHT = 800
+FPS = 60
+GRAVITY = .05
 
-root.title("Dih Destroyer")
-root.geometry(f"{width-10}x{height-10}")
-root.config(bg = "lightblue")
+#player settings/stats
+
+playerX = WIDTH/2
+playerY = HEIGHT/2
+playerW = 25
+playerH = 25
+playerVelX = 0
+playerVelY = 0
+PLAYERTERMVEL = 3
+playerSpeed = 5
+playerJumpHeight = 5
+jumping = False
+isGrounded = False
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+#pygame.display.set_caption("Dih Destroyer 2: The Suckqul")
+pygame.display.set_caption("Window")
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+
+    keys = pygame.key.get_pressed()
 
 
-label = tk.Label(root, text="Click the button?", font=("papyrus", 10, "bold"))
-label.pack(pady=20)
-button = tk.Button(
-    root,
-    text="click me UwU",
-    command=onClick,
-    bg="gray",
-    fg="white",
-    font=("papyrus", 10, "bold")
-)
+    if debugging:
+        print(playerY)
+        print(playerVelY)
+        if(keys[pygame.K_d] or keys[pygame.K_a]):
+            print("Keys are being pressed")
 
-button.pack(pady=20)
+    clock.tick(FPS)
+    print()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #make sure ts is actually running
+            running == False
 
-root.mainloop()
+    
+    
+    if keys[pygame.K_a]:
+        playerX -= playerVelX
+
+    if keys[pygame.K_d]:
+        playerX += playerVelX
+
+    if not jumping and [pygame.K_SPACE]:
+        playerVelY = playerJumpHeight
+        jumping = True
+
+    if playerVelY >= PLAYERTERMVEL:
+        playerVelY = PLAYERTERMVEL
+
+    if playerX < 0:
+        playerX = 0
+
+    if playerX > WIDTH:
+       playerX = WIDTH
+
+    playerVelY += GRAVITY
+    playerY += playerVelY
+
+    if playerY >= HEIGHT:
+       playerY = HEIGHT - playerH
+       playerVelY = 0
+       jumping = False
+       GRAVITY = 0
+       isGrounded = True
+    screen.fill((29, 220, 224))
+    if isGrounded:
+        playerVelY = 0
+
+    pygame.draw.rect(screen, (16, 32, 16), (playerX, playerY, playerH, playerW))
+
+    pygame.display.update()
+
+pygame.quit()
+
+
